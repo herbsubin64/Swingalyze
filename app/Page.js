e5a855bcf217f48ef97d38f04f5de908be623f47
herbsubin64 <hsubin@gmail.com>
2025-08-18 10:14:36 -0400

    Update Page.js

--- app/Page.js
+++ app/Page.js
@@ -1,3 +1,9 @@
-export default function Page() {
-  return <main style={{padding:24}}><h1>Swingalyze</h1></main>;
+// app/page.js
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Swingalyze</h1>
+      <p>If you can see this, the App Router is working.</p>
+    </main>
+  );
 }
